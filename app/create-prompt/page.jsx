b993236969cefc72a771a474d11de629898bec4d
Ahmed Abd ELGhany <am5419@fayoum.edu.eg>
2024-10-13 02:13:33 +0300

    create prompt

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,8 +1,56 @@
-import React from 'react'
+'use client'
+
+import { useState } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+import Form from "@components/Form";
 
 const CreatePrompt = () => {
+    const router = useRouter();
+    const { data: session } = useSession();
+    const [submitting, setSubmitting] = useState(false);
+    const [post, setPost] = useState({
+        prompt: '',
+        tag: '',
+    });
+
+
+    const createPrompt = async (e) => {
+        // prevent reload
+        e.preventDefault();
+        setSubmitting(true);
+        try {
+            const res = await fetch('/api/prompt/new', {
+                method: 'POST',
+                body: JSON.stringify({
+                    prompt: post.prompt,
+                    userId: session.user.id,
+                    tag: post.tag
+                })
+            })
+
+            if (res.ok) {
+                router.push('/')
+            }
+        } catch (err) {
+            console.log(err.message);
+
+        }
+        finally {
+            setSubmitting(false);
+        }
+
+    }
+
     return (
-        <div>CreatePrompt</div>
+        <Form
+            post={post}
+            type="Create"
+            setPost={setPost}
+            submitting={submitting}
+            handleSubmit={createPrompt}
+        />
     )
 }
 
